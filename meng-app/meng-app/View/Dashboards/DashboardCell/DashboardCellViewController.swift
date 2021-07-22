7e3679e3b258181c20bf61d68dd97afc5d3ca52c
Tassja <Hannatassja@student.umn.ac.id>
2021-07-22 22:33:36 +0700

    Modified Folder

---
+++ meng-app/meng-app/View/Dashboards/DashboardCell/DashboardCellViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DashboardCellViewController.swift
+//  meng-app
+//
+//  Created by Hannatassja Hardjadinata on 22/07/21.
+//
+
+import UIKit
+
+class DashboardCellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
