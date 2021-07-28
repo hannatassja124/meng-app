f23a79e46477fa0d7225824cd7d5e6293ebab409
Arya <aryailham05@gmail.com>
2021-07-28 22:24:26 +0700

    ADD: Function to fetch data from CoreData

--- meng-app/meng-app/Controller/Cats/CatsViewController.swift
+++ meng-app/meng-app/Controller/Cats/CatsViewController.swift
@@ -20,11 +20,27 @@ class CatsViewController: UIViewController, UICollectionViewDelegate {
 
         // Do any additional setup after loading the view.
         
+        retrieveData()
+        
         catsCollectionView.delegate = self
         catsCollectionView.dataSource = self
         
         initNib()
+        
+        print(cats.count)
     }
+    
+//    private func genDummyData(){
+//        let cat = Cats(context: context)
+//
+//        cat.name = "udin"
+//
+//        do {
+//            try context.save()
+//        } catch {
+//            //error
+//        }
+//    }
     
     private func initNib(){
         
@@ -50,37 +66,46 @@ class CatsViewController: UIViewController, UICollectionViewDelegate {
 }
 
 extension CatsViewController: UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return cats.count + 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         if indexPath.row == 0 {
             return collectionView.dequeueReusableCell(withReuseIdentifier: "AddNewCatCell", for: indexPath) as! AddNewCatCollectionViewCell
         }
         else{
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CatProfileCell", for: indexPath) as! CatProfileCollectionViewCell
             
-            /* assign data ke CollectionView
-            if let image = cats[indexPath.row].image {
-                cell.petImage.image = UIImage(data: image)
-            }
-            cell.petNameLabel.text = "\(cats[indexPath.row].name!)"
-            cell.petGenderIcon.image =
+//             assign data ke CollectionView
+//            if let image = cats[indexPath.row - 1].image {
+//                cell.petImage.image = UIImage(data: image)
+//            }
+            cell.petNameLabel.text = "\(cats[indexPath.row - 1].name!)"
+            cell.petGenderIcon.image = UIImage(named: (cats[indexPath.row - 1].gender == 0 ? "Male" : "Female"))
             cell.petTagsColor.tintColor = .red
-
-            */
+            
+//            cell.petNameLabel.text = "test"
+            
             return cell
         }
     }
     
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
         if indexPath.row == 0{
             //bakal buka Add New Cat Page
+            print("add new cat")
         }
         else{
+            print("cat detail")
             //bakal buka Cat Profile Detail
+            let CatDetailViewController = CatDetailViewController()
+            self.navigationController?.pushViewController(CatDetailViewController, animated: true)
         }
     }
     
