ae2a6ffaf2aa0d6374df40a53b75d575167ca03d
Adinda Puji R <adindapujir@gmail.com>
2021-07-30 20:07:47 +0700

    ADD: Date Picker, Breed, and Storyboard
    - Add Date Picker Feature
    - Add Breed Data (Not feature yet)
    - Fixing storyboard
    - Table View Cell hide and unhide

--- meng-app/meng-app/Controller/AddNewCat/AddNewCatTableView.swift
+++ meng-app/meng-app/Controller/AddNewCat/AddNewCatTableView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddNewCatTableView: UITableViewController {
+class AddNewCatTableView: UITableViewController, UIPickerViewDelegate {
     
     // Image
     @IBOutlet weak var ncCatPhotoImage: UIImageView!
@@ -36,8 +36,11 @@ class AddNewCatTableView: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         nameField()
-
+        ncCatDOBDate()
+        hiddenPickers(fieldName: "init", indexPath: [-1])
     }
+    
+    // Untuk Name Text Field
     
     func nameField() {
         
@@ -51,5 +54,109 @@ class AddNewCatTableView: UITableViewController {
         ncCatNameTF.textAlignment = .center
     }
     
+    // Picker Data
+    var pickerData:[String] = [String]()
+    
+    func pickerDataFill() {
+        
+        self.ncCatBreedPicker.delegate = self
+//        self.ncCatBreedPicker.dataSource = self
+//
+        pickerData = ["Abyssinian", "American Bobtail", "American Curl", "American Shorthair", "American Wirehair", "Balinese-Javanese", "Bengal", "Birman", "Bombay", "British Shorthair", "Burmese", "Chartreux", "Cornish Rex", "Devon Rex", "Egyptian Mau", "European Burmese", "Exotic Shorthair", "Havana Brown", "Himalayan", "Japanese Bobtail", "Korat", "LaPerm", "Maine Coon", "Manx", "Munchkin", "Norwegian Forest", "Ocicat", "Oriental", "Persian", "Peterbald", "Pixiebob", "Ragamuffin", "Ragdoll", "Russian Blue", "Savannah", "Scottish Fold", "Selkirk Rex", "Siamese", "Siberian", "Singapura", "Somali", "Sphynx", "Tonkinese", "Toyger", "Turkish Angora", "Turkish Van"]
+    }
+    
+    // Format Date to String untuk ditampilkan ke Date Label
+    @IBAction func ncCatDOBPickerAction(_ sender: Any) {
+        ncCatDOBLabel.text = dateFormat(date: ncCatDOBPicker.date, formatDate: dateFormatTemp)
+    }
+    
+    func ncCatDOBDate () {
+        ncCatDOBLabel.text = dateFormat(date: ncCatDOBPicker.date, formatDate: dateFormatTemp)
+    }
+    
+    let dateFormatTemp =  "MMMM dd, yyyy"
+    
+    func dateFormat(date: Date, formatDate: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = formatDate
+        return dateFormatter.string(from: date)
+    }
+    
+    
+    // Table View Display
+    
+    func hiddenPickers(fieldName: String, indexPath: IndexPath) {
+        ncCatColorTagsPicker.isHidden = fieldName == "catColorTags" ? !ncCatColorTagsPicker.isHidden : true
+        ncCatGenderPicker.isHidden = fieldName == "catGender" ? !ncCatGenderPicker.isHidden : true
+        ncCatDOBPicker.isHidden = fieldName == "catDOB" ? !ncCatDOBPicker.isHidden : true
+        ncCatBreedPicker.isHidden = fieldName == "catBreed" ? !ncCatBreedPicker.isHidden : true
+        ncCatNeuteredPicker.isHidden = fieldName == "catNeutered" ? !ncCatNeuteredPicker.isHidden : true
+        
+        animation(oniP: indexPath)
+    }
+    
+    func animation (oniP : IndexPath){
+        UIView.animate(withDuration:0.3, animations: { () -> Void in
+        self.tableView.beginUpdates()
+        self.tableView.deselectRow(at: oniP, animated: true)
+        self.tableView.endUpdates()
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let cellCatColorTagsPicker = indexPath.section == 1 && indexPath.row == 1
+        let cellCatGenderPicker = indexPath.section == 2 && indexPath.row == 1
+        let cellCatDOBPicker = indexPath.section == 3 && indexPath.row == 1
+        let cellCatBreedPicker = indexPath.section == 4 && indexPath.row == 1
+        let cellCatNeuteredPicker = indexPath.section == 5 && indexPath.row == 1
+        
+        var ncHeight: CGFloat = 43.5
+        
+        if (cellCatColorTagsPicker && ncCatColorTagsPicker.isHidden) || (cellCatGenderPicker && ncCatGenderPicker.isHidden) || (cellCatDOBPicker && ncCatDOBPicker.isHidden) || (cellCatBreedPicker && ncCatBreedPicker.isHidden) || (cellCatNeuteredPicker && ncCatNeuteredPicker.isHidden) {
+            ncHeight = 0.0
+        }
+        if (cellCatColorTagsPicker && !ncCatColorTagsPicker.isHidden) || (cellCatGenderPicker && !ncCatGenderPicker.isHidden) || (cellCatBreedPicker && !ncCatBreedPicker.isHidden) || (cellCatNeuteredPicker && !ncCatNeuteredPicker.isHidden) {
+            ncHeight = 150.0
+        }
+        if (cellCatDOBPicker && !ncCatDOBPicker.isHidden) {
+            ncHeight = 320.0
+        }
+        return ncHeight
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let ncCatColorTagsIP = NSIndexPath (row: 0, section: 1)
+        let ncCatGenderIP = NSIndexPath (row: 0, section: 2)
+        let ncCatDOBIP = NSIndexPath (row: 0, section: 3)
+        let ncCatBreedIP = NSIndexPath (row: 0, section: 4)
+        let ncCatNeuteredIP = NSIndexPath (row: 0, section: 5)
+        
+        if ncCatColorTagsIP as IndexPath == indexPath {
+            hiddenPickers(fieldName: "catColorTags", indexPath: indexPath)
+        }
+        else if ncCatGenderIP as IndexPath == indexPath {
+            hiddenPickers(fieldName: "catGender", indexPath: indexPath)
+        }
+        else if ncCatDOBIP as IndexPath == indexPath {
+            hiddenPickers(fieldName: "catDOB", indexPath: indexPath)
+        }
+        else if ncCatBreedIP as IndexPath == indexPath {
+            hiddenPickers(fieldName: "catBreed", indexPath: indexPath)
+        }
+        else if ncCatNeuteredIP as IndexPath == indexPath {
+            hiddenPickers(fieldName: "catNeutered", indexPath: indexPath)
+        }
+    }
     
 }
+
+//extension AddNewCatTableView: UIPickerViewDataSource {
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//
+//    }
+//
+//}
