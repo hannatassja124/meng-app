ac1aa7327a98339970cfbb98bf3331b5866a3b9d
Arya <aryailham05@gmail.com>
2021-08-05 09:47:52 +0700

    ADD: Render data to the UI on CatProfileDetail

--- meng-app/meng-app/Controller/Cat Detail/CatDetailViewController.swift
+++ meng-app/meng-app/Controller/Cat Detail/CatDetailViewController.swift
@@ -8,13 +8,40 @@
 import UIKit
 
 class CatDetailViewController: UIViewController {
-    
+    //selected cat model
     var currCat: Cats?
-
+    
+    //IBOutlet
+    @IBOutlet weak var catImage: UIImageView!
+    @IBOutlet weak var catName: UILabel!
+    @IBOutlet weak var catGenderIcon: UIImageView!
+    @IBOutlet weak var catColorTags: UIImageView!
+    @IBOutlet weak var catBreedAndNeutered: UILabel!
+    @IBOutlet weak var catAge: UILabel!
+    @IBOutlet weak var catWeight: UILabel!
+    @IBOutlet weak var catFood: UILabel!
+    @IBOutlet weak var catMedicalNotes: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        assignDatatoPage()
         // Do any additional setup after loading the view.
+    }
+    
+    func assignDatatoPage() {
+        if let image = currCat?.image {
+            catImage.image = UIImage(data: image)
+        }
+        catName.text = currCat?.name ?? "Cat"
+        catGenderIcon.image = UIImage(named: (currCat!.gender == 0 ? "Male" : "Female"))
+        catColorTags.tintColor = TagsHelper.checkColor(tagsNumber: currCat!.colorTags)
+        let neuteredString = currCat!.isNeutered ? "neutered" : "not neutered"
+        catBreedAndNeutered.text = "\(currCat!.breed) , \(neuteredString)"
+        catAge.text = "2 years"
+        catWeight.text = "\(currCat!.weight) KG"
+        catFood.text = "\(currCat?.feeding ?? "no data")"
+        catMedicalNotes.text = "\(currCat?.notes ?? "no data")"
     }
     
 
