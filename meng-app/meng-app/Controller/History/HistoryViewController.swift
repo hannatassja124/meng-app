6794dd71f9ff3cf0967032f2f37dce1944aa2ac1
Arya <aryailham05@gmail.com>
2021-08-07 23:24:57 +0700

    ADD: Function to get activity data from selected cat and render it to UI

--- meng-app/meng-app/Controller/History/HistoryViewController.swift
+++ meng-app/meng-app/Controller/History/HistoryViewController.swift
@@ -9,24 +9,94 @@ import UIKit
 
 class HistoryViewController: UIViewController {
     
-    var selectedCat = Cats()
+    let activitiesCellId = "ActivitiesTableViewCell"
 
+    
+    var selectedCat = Cats()
+    var sortedCatActivites = [Activity()]
+    @IBOutlet weak var activitiesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkIfActivitiesEmptyOrNot()
         
-        print(selectedCat.name)
-        // Do any additional setup after loading the view.
+        initiateTableView()
+
+    }
+    
+    func initiateTableView(){
+        activitiesTableView.delegate = self
+        activitiesTableView.dataSource = self
+        
+        self.activitiesTableView.register(UINib.init(nibName: activitiesCellId, bundle: nil), forCellReuseIdentifier: activitiesCellId)
+        activitiesTableView.separatorColor = .clear
+        
+    }
+    
+    func checkIfActivitiesEmptyOrNot(){
+        if(selectedCat.activities?.count == 0){
+            activitiesTableView.isHidden = true
+        }
+        else{
+            sortActivitiesByDate()
+        }
+    }
+    
+    func sortActivitiesByDate(){
+        let catActivites = selectedCat.activities?.allObjects as! [Activity]
+        print(catActivites)
+        sortedCatActivites = catActivites
+    }
+    
+    
+    @IBAction func back(_ sender: Any) {
+        if let navController = self.navigationController {
+            for controller in navController.viewControllers {
+                if controller is CatDetailViewController { // Change to suit your menu view controller subclass
+                    navController.popToViewController(controller, animated:true)
+                    break
+                }
+            }
+        }
+    }
+    
+}
+
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sortedCatActivites.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: activitiesCellId, for: indexPath) as! ActivitiesTableViewCell
+        
+        let act = sortedCatActivites[indexPath.row]
+        cell.activityTitleLabel.text = "\(act.activityTitle!)"
+        
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "MMMM dd, YYYY"
+        cell.activityTimeLabel.text = "\(dateformatter.string(from: act.activityDateTime!))"
+        
+        cell.activityCatNameLabel.text = "\(selectedCat.name!)"
+        cell.activityTypeImage.image = UIImage(systemName: "stethoscope")
 
-    /*
-    // MARK: - Navigation
+        cell.selectionStyle = .none
+        
+        return cell
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "ActivityDetail", bundle: nil)
+       
+        let vc = storyboard.instantiateViewController(withIdentifier: "ActivityDetailStoryboard") as! ActivityDetailViewController
+        let nc = UINavigationController(rootViewController: vc)
+        nc.navigationBar.isTranslucent = false
+        nc.navigationBar.barTintColor = #colorLiteral(red: 0.1036602035, green: 0.2654651999, blue: 0.3154058456, alpha: 1)
+        
+        self.present(nc, animated: true, completion: nil)
+
+    }
+
 
 }
