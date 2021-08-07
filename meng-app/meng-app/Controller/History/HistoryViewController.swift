70d5ffb2d7f4d9e407ee2e4d2469f6d2a7432377
Arya <aryailham05@gmail.com>
2021-08-07 20:00:13 +0700

    ADD: view if there is no data on history page

---
+++ meng-app/meng-app/Controller/History/HistoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryViewController.swift
+//  meng-app
+//
+//  Created by Arya Ilham on 07/08/21.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
